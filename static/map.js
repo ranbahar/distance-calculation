e345db1d63626c496f81bf57aeb9155c4c74bc39
ranbahar <ran.bahar@paymeservice.com>
2023-10-29 15:19:57 +0200

    Add multiple points and show them on the map

--- static/map.js
+++ static/map.js
@@ -1,6 +1,12 @@
 var map;
 var circle;
 var marker;
+var points = [
+    { lat: 32.08519345111358, lng: 34.77496212125395 },
+    { lat: 32.09519345111358, lng: 34.78496212125395 },
+    { lat: 32.10519345111358, lng: 34.79496212125395 }
+    // ... add more points as needed
+    ];
 
 function initialize() {
     var mapOptions = {
@@ -12,11 +18,11 @@ function initialize() {
 
     // Set marker on default position
     var defaultPosition = new google.maps.LatLng(32.07519345111358, 34.77496212125395);
-    setMarker(defaultPosition);
-
-
-    // Draw a circle on default position
-    placeCircle(defaultPosition);
+//    setMarker(defaultPosition);
+//
+//
+//    // Draw a circle on default position
+//    placeCircle(defaultPosition);
 
     map.addListener('click', function(event) {
         placeCircle(event.latLng);
@@ -26,6 +32,19 @@ function initialize() {
         map.addListener('contextmenu', function(event) {
         showLatLng(event.latLng);
     });
+
+    placeMarkers();
+}
+
+function placeMarkers() {
+    for (var i = 0; i < points.length; i++) {
+        var point = points[i];
+        var location = new google.maps.LatLng(point.lat, point.lng);
+        new google.maps.Marker({
+            position: location,
+            map: map
+        });
+    }
 }
 
 function showLatLng(latLng) {
